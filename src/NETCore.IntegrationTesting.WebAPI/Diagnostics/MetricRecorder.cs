1bcb5ca0b68420fc566f01f012a57e53cf500955
Ankan <ankan.mookherjee@newday.co.uk>
2021-03-30 20:18:57 +0100

    adding middleware test for metrics

---
+++ src/NETCore.IntegrationTesting.WebAPI/Diagnostics/MetricRecorder.cs
@@ -0,0 +1,10 @@
+﻿namespace NETCore.IntegrationTesting.WebAPI.Diagnostics
+{
+    public class MetricRecorder : IMetricRecorder
+    {
+        public void RecordMetric(string name, int increment, string[] tags)
+        {
+            // does nothing for this demo, you can add datadog, prometheus or appinsights logic here
+        }
+    }
+}
---
+++ src/NETCore.IntegrationTesting.WebAPI/Middleware/MetricsMiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace NETCore.IntegrationTesting.WebAPI.Middleware
+{
+    public static class MetricsMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestMetrics(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<MetricsMiddleware>();
+        }
+    }
+}
